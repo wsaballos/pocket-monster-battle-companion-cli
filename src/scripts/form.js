629555875dba66fb3004c4e81594a7f9c71d88d5
Juan Monge <j1monge.mz@gmail.com>
2019-08-14 21:42:02 -0700

    add pokemon list to markup

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -4,6 +4,7 @@ $(function () {
     socket.emit('load');
     socket.on('load', function (numberOfConnections, id, listOfPokemon) {
         let user = socket.id === id;
+        let pokemonRow = document.getElementById("list-of-pokemon");
 
         if (numberOfConnections === 1) {
             console.log('Waiting for a trainer to connect')
@@ -14,7 +15,26 @@ $(function () {
                 console.log('A trainer is in the chat, and ready to battle.')
             }
         }
-    })
+        console.log(listOfPokemon)
+    // for loop for list of pokemon
+    for (var pokemonIndex = 0; pokemonIndex < 150; pokemonIndex++) {
+        let pokemonName = listOfPokemon[pokemonIndex].name;
+
+        pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+
+        if (pokemonName.slice(pokemonName.length - 2, pokemonName.length) === '-m') {
+            pokemonName = pokemonName.slice(0, pokemonName.length - 2) + ' male'; 
+        } else if (pokemonName.slice(pokemonName.length - 2, pokemonName.length) === '-f') {
+            pokemonName = pokemonName.slice(0, pokemonName.length - 2) + ' female';
+        }
+
+        pokemonRow.innerHTML += `<tr>
+                                    <th scope="row">${pokemonIndex + 1}</th>
+                                    <td>${pokemonName}</td>
+                                </tr>`;
+        listOfPokemon[pokemonIndex];    
+    }
+})
 
     $('form').submit(function (e) {
         e.preventDefault(); // prevents page reloading
